d804dd535c02787f9d0da4c8267ee8ef4f114f69
HannesOberreiter <hoberreiter@gmail.com>
2023-06-01 12:26:49 +0200

    test: :white_check_mark: aws infra and jest test

---
+++ test/index.spec.js
@@ -0,0 +1,73 @@
+const AwsSignRequest = require('../index.js');
+const superagent = require('superagent');
+
+require('dotenv').config();
+const ROLE_ARN = process.env.ROLE_ARN;
+const GATEWAY_URL = process.env.GATEWAY_URL;
+
+describe('AwsSignRequest', () => {
+  let signer = new AwsSignRequest();
+
+  beforeEach(() => {
+    signer = new AwsSignRequest();
+  });
+
+  it('should set and get credentials', () => {
+    const credentials = {
+      accessKeyId: 'ACCESS_KEY',
+      secretAccessKey: 'SECRET_ACCESS_KEY',
+      sessionToken: 'SESSION_TOKEN',
+    };
+
+    const response = signer.setCredentials(credentials);
+    expect(response).toEqual(credentials);
+  });
+
+  it('should set and get credentials from config', async () => {
+    const profile = 'default';
+    const response = await signer.setCredentialsFromConfig(profile);
+    expect(response).toBeDefined();
+  });
+
+  it('should assume a role and set session credentials', async () => {
+    const roleParams = {
+      RoleArn: ROLE_ARN,
+      RoleSessionName: 'TestSession',
+    };
+
+    await signer.assumeRole(roleParams);
+    expect(signer.session).toBeDefined();
+  });
+
+  it('should remove the assumed role after assuming it', async () => {
+    const roleParams = {
+      RoleArn: ROLE_ARN,
+      RoleSessionName: 'TestSession',
+    };
+
+    await signer.assumeRole(roleParams);
+    expect(signer.session).toBeDefined();
+
+    signer.removeRole();
+    expect(signer.session).toBeNull();
+  });
+
+  it('should set and get the region', () => {
+    const region = 'us-west-2';
+
+    signer.setRegion(region);
+    expect(signer.region).toEqual(region);
+  });
+
+  it('should create a custom end function and sign the request', async () => {
+    const roleParams = {
+      RoleArn: ROLE_ARN,
+      RoleSessionName: 'TestSession',
+    };
+
+    await signer.assumeRole(roleParams);
+    const response = await superagent.get(GATEWAY_URL).use(signer.add());
+
+    expect(response.status).toEqual(200);
+  });
+});
